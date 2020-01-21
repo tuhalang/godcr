626ecfffbb5af7fe68555d94f7492aaf1094ccde
Osho Klinsmann <oshoklinsmann@gmail.com>
2020-01-21 13:08:57 +0100

    layout sync status box

---
+++ ui/values/dimensions.go
@@ -0,0 +1,8 @@
+package values
+
+// EntireSpace is the value for the weight of a FlexChild
+var EntireSpace = float32(1)
+
+// DefaultProgressBarHeight is the default height for progress bar
+// when non is specified.
+var DefaultProgressBarHeight = 20
