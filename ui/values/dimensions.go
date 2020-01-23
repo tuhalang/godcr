558a7ece9c9a1f78e408d3f9e387327ad3b2ba28
Osho Klinsmann <oshoklinsmann@gmail.com>
2020-01-23 13:19:52 +0100

    add list layout for wallet sync boxes

--- ui/values/dimensions.go
+++ ui/values/dimensions.go
@@ -6,3 +6,6 @@ var EntireSpace = float32(1)
 // DefaultProgressBarHeight is the default height for progress bar
 // when non is specified.
 var DefaultProgressBarHeight = 20
+
+var SyncBoxWidthMin = 500
+var SyncBoxWidthMax = 550
