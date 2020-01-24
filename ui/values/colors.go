0bec031a0fbd80d176f1d30f4a94d6a11afa547e
Osho Klinsmann <oshoklinsmann@gmail.com>
2020-01-24 14:01:31 +0100

    add single declaration to values

--- ui/values/colors.go
+++ ui/values/colors.go
@@ -2,11 +2,13 @@ package values
 
 import "image/color"
 
-//  ProgressBarGray indicates the level of sync progress that is yet to be completed.
-var ProgressBarGray = color.RGBA{230, 234, 237, 255}
+var (
+	//  ProgressBarGray indicates the level of sync progress that is yet to be completed.
+	ProgressBarGray = color.RGBA{230, 234, 237, 255}
 
-// ProgressBarGreen indicates the level of sync progress that has been completed.
-var ProgressBarGreen = color.RGBA{65, 190, 83, 255}
+	// ProgressBarGreen indicates the level of sync progress that has been completed.
+	ProgressBarGreen = color.RGBA{65, 190, 83, 255}
 
-// walletSyncBoxGray is the background color of wallet sync boxes.
-var WalletSyncBoxGray = color.RGBA{243, 245, 246, 255}
+	// walletSyncBoxGray is the background color of wallet sync boxes.
+	WalletSyncBoxGray = color.RGBA{243, 245, 246, 255}
+)
--- ui/values/dimensions.go
+++ ui/values/dimensions.go
@@ -1,9 +1,11 @@
 package values
 
-// EntireSpace is the value for the weight of a FlexChild
-var EntireSpace = float32(1)
+var (
+	// EntireSpace is the value for the weight of a FlexChild
+	EntireSpace = float32(1)
 
-// DefaultProgressBarHeight is the default height for progress bar
-// when non is specified.
-var DefaultProgressBarHeight = 20
+	// DefaultProgressBarHeight is the default height for progress bar
+	// when non is specified.
+	DefaultProgressBarHeight = 20
+)
 
