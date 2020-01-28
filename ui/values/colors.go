fc6785ed3cb238ac5cb0fd6dbd2366e2eb58eb28
Osho Klinsmann <oshoklinsmann@gmail.com>
2020-01-28 15:53:07 +0100

    added transparent sync button

--- ui/values/colors.go
+++ ui/values/colors.go
@@ -12,5 +12,9 @@ var (
 	// walletSyncBoxGray is the background color of wallet sync boxes.
 	DefaultCardGray = color.RGBA{243, 245, 246, 255}
 
-	Transparent = color.RGBA{0, 0, 0, 0}
+	// Transparent is the RGBA value for no color
+	ButtonGray = color.RGBA{196, 203, 210, 255}
+
+	// White is the RGBA value for white color
+	White = color.RGBA{255, 255, 255, 255}
 )
--- ui/values/dimensions.go
+++ ui/values/dimensions.go
@@ -7,5 +7,13 @@ var (
 	// DefaultProgressBarHeight is the default height for progress bar
 	// when non is specified.
 	DefaultProgressBarHeight = 20
-)
 
+	// SyncButtonHeight is the height for the button used to trigger sync actions
+	SyncButtonHeight = 67
+
+	// SyncButtonWidth is the height for the button used to trigger sync actions
+	SyncButtonWidth = 108
+
+	// SyncButtonWidth is the thickness of the button borders.
+	SyncButtonBorderWidth = 2
+)
